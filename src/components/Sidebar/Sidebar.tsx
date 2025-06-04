d40e1f5daae08a71e9a3348c43ca2e8c16941fff
HIPEHIPEHIPE <taijipjs@naver.com>
2025-06-04 10:14:08 +0900

    0604_1014

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,16 +8,17 @@ const Sidebar = () => {
       color: 'white',
       padding: '20px 16px',
       boxSizing: 'border-box',
-      height: '100vh'
+      height: '130vh'
     }}>
-      <h3 style={{ marginBottom: 24 }}>🧪 ChemiGuard</h3>
+      <h3 style={{ marginBottom: 24 }}>🧪 ChemiGuard v1.0</h3>
       <ul style={{ listStyle: 'none', padding: 0 }}>
-        <li style={{ marginBottom: 12 }}>대시보드</li>
-        <li style={{ marginBottom: 12 }}>데이터 수집</li>
+        <li style={{ marginBottom: 12 }}>관리자 대시보드</li>
+        <li style={{ marginBottom: 12 }}>데이터 획득</li>
         <li style={{ marginBottom: 12 }}>데이터 관리</li>
         <li style={{ marginBottom: 12 }}>데이터 정제</li>
-        <li style={{ marginBottom: 12 }}>QA 생성</li>
-        <li style={{ marginBottom: 12 }}>QA 검수</li>
+        <li style={{ marginBottom: 12 }}>데이터 가공</li>
+        <li style={{ marginBottom: 12 }}>Q&A 생성</li>
+        <li style={{ marginBottom: 12 }}>데이터 검수</li>
       </ul>
     </div>
   );
